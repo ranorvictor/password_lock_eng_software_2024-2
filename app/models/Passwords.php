d59e78fa3ccd91bd9c52f816580197e7f5d79276
rafaelpacinii <rafael_pmv@hotmail.com>
2024-08-20 20:47:49 -0300

    Switching the project structure and adding connection to database

---
+++ app/models/Passwords.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\models;
+
+
+use app\models\Model;
+
+class Passwords extends Model
+{
+  protected $table = "passwords";
+}
---
+++ app/views/layout.php
@@ -0,0 +1,18 @@
+<DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <header>
+      Header
+      <a href='/homePage'>Home</a>
+      <a href='?controller=xyz&action=index'>Posts</a>
+    </header>
+
+    <?php require_once('routes.php'); ?>
+
+    <footer>
+      Footer
+    </footer>
+  <body>
+<html>

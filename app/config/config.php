b3333f0cfaf8e9c47a8da4c526a6579c84eeb5b0
rafaelpacinii <rafael_pmv@hotmail.com>
2024-08-20 23:11:35 -0300

    Changing database logic

---
+++ app/config/config.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Config;
+
+use mysqli;
+
+class Conexao
+{
+    function conectarBancoDeDados()
+    {
+        $dbhost = 'localhost';
+        $dbUsername = 'root';
+        $dbPassword = '';
+        $dbName = 'passwordlock';
+
+        $conexao = new mysqli($dbhost, $dbUsername, $dbPassword, $dbName);
+
+        if ($conexao->connect_errno) {
+            die("Erro na conexão com o banco de dados: " . $conexao->connect_error);
+        }
+
+        return $conexao;
+    }
+}
